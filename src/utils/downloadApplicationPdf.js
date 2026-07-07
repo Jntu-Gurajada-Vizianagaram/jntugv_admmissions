2922ba2a7538e3190f691f4322179025caa50cb4
anilsinthu114 <anilsinthu114@gmail.com>
2026-07-07 10:30:18 +0530

    Export application PDF as single A4 page

--- src/utils/downloadApplicationPdf.js
+++ src/utils/downloadApplicationPdf.js
@@ -19,7 +19,7 @@ export const downloadApplicationPdf = async (registrationNo = '') => {
 
   const canvas = await html2canvas(element, {
     backgroundColor: '#ffffff',
-    scale: 2,
+    scale: 3,
     useCORS: true,
     logging: false,
     windowWidth: element.scrollWidth,
@@ -29,42 +29,7 @@ export const downloadApplicationPdf = async (registrationNo = '') => {
   const pdf = new jsPDF('p', 'mm', 'a4')
   const pageWidth = pdf.internal.pageSize.getWidth()
   const pageHeight = pdf.internal.pageSize.getHeight()
-  const margin = 8
-  const contentWidth = pageWidth - (margin * 2)
-  const contentHeight = pageHeight - (margin * 2)
-  const pxPerMm = canvas.width / contentWidth
-  const pageCanvasHeight = Math.floor(contentHeight * pxPerMm)
-  let renderedHeight = 0
-
-  while (renderedHeight < canvas.height) {
-    const sliceHeight = Math.min(pageCanvasHeight, canvas.height - renderedHeight)
-    const pageCanvas = document.createElement('canvas')
-    pageCanvas.width = canvas.width
-    pageCanvas.height = sliceHeight
-
-    const context = pageCanvas.getContext('2d')
-    context.fillStyle = '#ffffff'
-    context.fillRect(0, 0, pageCanvas.width, pageCanvas.height)
-    context.drawImage(
-      canvas,
-      0,
-      renderedHeight,
-      canvas.width,
-      sliceHeight,
-      0,
-      0,
-      canvas.width,
-      sliceHeight
-    )
-
-    if (renderedHeight > 0) {
-      pdf.addPage()
-    }
-
-    const imageHeight = sliceHeight / pxPerMm
-    pdf.addImage(pageCanvas.toDataURL('image/jpeg', 0.95), 'JPEG', margin, margin, contentWidth, imageHeight)
-    renderedHeight += sliceHeight
-  }
+  pdf.addImage(canvas.toDataURL('image/jpeg', 0.98), 'JPEG', 0, 0, pageWidth, pageHeight)
 
   pdf.save(`${filenameSafe(registrationNo || 'JNTUGV-IIBMP-Application')}.pdf`)
 }
