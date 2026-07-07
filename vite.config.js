de83478e2b0c3991e841d7c452a1fff6d1ebfa15
anilsinthu114 <anilsinthu114@gmail.com>
2026-07-07 13:13:18 +0530

    Allow admissions domain in Vite dev server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['admissions.jntugv.edu.in'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
